117a7fbf00065a172d2523aa9264ef0e1435d75d
Moustapha Touré <moostoure@gmail.com>
2022-06-21 19:17:49 +0200

    commit initialisation projet
    -création react-app
    - Epuration
    - Ajout de packages axios | node-sass |  redux | react-redux @reduxjs/toolkit  | react-router-dom | dotenv
    - Création dossiers components + components (base react) | features (redux files) | common pour API key (masquée) + API calls

---
+++ movies-list/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';  // Mise en place du routing dans app
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // import pour mise en place du routing
+
+import './App.sass';  // style
+
+// import de nos components
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer'
+
+
+
+function App() {
+  return (
+    // application
+      <div className="App">
+        <Router> 
+          <Header/>
+          <Routes>
+              <Route 
+                element={<Home/>}
+                path="/" 
+              />
+              <Route 
+                element={<MovieDetail/>}
+                path='/movie/:imdbID' 
+              />
+              <Route 
+                element={<PageNotFound/>}
+                path="*"
+              />
+          </Routes>
+          <Footer/>
+        </Router>
+      </div>
+    // Fin application
+  );
+}
+
+export default App;
---
+++ movies-list/src/common/apis/MovieApiKey.js
@@ -0,0 +1,7 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const APIkey = process.env.key;
+
+
+// clé fournie par omdbApi
